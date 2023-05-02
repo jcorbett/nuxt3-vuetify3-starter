c751a8763114dde6219d03822151ef67c8a78d8a
Justin Corbett <justin.a.corbett@gmail.com>
2023-05-02 10:05:28 -0700

    add icons/font

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -2,12 +2,27 @@
 // vite-plugin-vuetify auto imports components and directives
 import { createVuetify } from 'vuetify'
 
+import { aliases, fa } from 'vuetify/iconsets/fa'
+import {mdi} from "vuetify/lib/iconsets/mdi";
+
+import '@mdi/font/css/materialdesignicons.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+
 export default defineNuxtPlugin(nuxtApp => {
     const vuetify = createVuetify({
         // config vuetify here
         theme: {
             defaultTheme: 'dark'
-        }
+        },
+        icons: {
+            defaultSet: 'mdi',
+            aliases,
+            sets: {
+                mdi,
+                fa
+            }
+        },
+
     })
 
     nuxtApp.vueApp.use(vuetify)
